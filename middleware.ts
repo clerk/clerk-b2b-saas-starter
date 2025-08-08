1d6662d98b9fa558491e735b28f4873c1b4d154e
Kevin Wang <26389321+thiskevinwang@users.noreply.github.com>
2025-08-08 17:33:42 -0400

    chore: add burner PK (#2)

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
+import { PUBLISHABLE_KEY } from '@/contants/keys';
+
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
-});
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect();
+  },
+  // https://clerk.com/docs/references/nextjs/clerk-middleware#dynamic-keys
+  () => {
+    return {
+      publishableKey: PUBLISHABLE_KEY,
+      secretKey: 'sk_test_gototheclerkdashboard',
+    };
+  },
+);
 
 export const config = {
   matcher: [
